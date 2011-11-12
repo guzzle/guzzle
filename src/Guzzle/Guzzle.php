bc8dca93af9b0ce9e174ae81ccaa0cbb6dacc66e
Michael Dowling <mtdowling@gmail.com>
2011-11-12 17:37:59 -0600

    Adding check to see if curl is installed.  Closes #9

--- src/Guzzle/Guzzle.php
+++ src/Guzzle/Guzzle.php
@@ -64,7 +64,12 @@ class Guzzle
     public static function getCurlInfo($type = null)
     {
         if (!self::$curl) {
-            self::$curl = curl_version();
+            self::$curl = @curl_version();
+            // @codeCoverageIgnoreStart
+            if (!self::$curl) {
+                throw new \RuntimeException('Guzzle requires curl');
+            }
+            // @codeCoverageIgnoreEnd
             // Check if CURLOPT_FOLLOWLOCATION is available
             self::$curl['follow_location'] = !ini_get('open_basedir');
         }
