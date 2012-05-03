48c5668a86e3705262768d82bde565fb7742fa90
Peter Johnson <insomnia@rcpt.at>
2012-05-03 11:04:48 +0100

    Reverted test to type hint interfaces

--- tests/Guzzle/Tests/Http/Message/ResponseTest.php
+++ tests/Guzzle/Tests/Http/Message/ResponseTest.php
@@ -6,6 +6,7 @@ use Guzzle\Guzzle;
 use Guzzle\Common\Collection;
 use Guzzle\Common\Exception\InvalidArgumentException;
 use Guzzle\Http\EntityBody;
+use Guzzle\Http\HttpException;
 use Guzzle\Http\Exception\BadResponseException;
 use Guzzle\Http\Message\Response;
 use Guzzle\Http\Message\RequestFactory;
@@ -82,7 +83,7 @@ class ResponseTest extends \Guzzle\Tests\GuzzleTestCase
         try {
             //$response = new Response(200, null, array('foo' => 'bar'));
             //$this->fail('Response did not throw exception when passing invalid body');
-        } catch(\Exception $e) {
+        } catch (HttpException $e) {
         }
 
         // Make sure the proper exception is thrown when sending invalid code
