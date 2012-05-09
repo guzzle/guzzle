bfb4be41447961cb2f5e05e94cf9fe44ed37e0ed
Michael Dowling <mtdowling@gmail.com>
2012-05-09 15:52:04 -0700

    Removing references to Symfony2 validator.  Changing phar alias from Guzzle to guzzle.

--- phar-stub.php
+++ phar-stub.php
@@ -1,14 +1,13 @@
 <?php
 
-require_once 'phar://Guzzle/vendor/symfony/class-loader/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+require_once 'phar://guzzle/vendor/symfony/class-loader/Symfony/Component/ClassLoader/UniversalClassLoader.php';
 
 $classLoader = new Symfony\Component\ClassLoader\UniversalClassLoader();
 $classLoader->registerNamespaces(array(
-    'Guzzle' => 'phar://Guzzle/src',
-    'Symfony\\Component\\Validator' => 'phar://Guzzle/vendor/symfony/validator',
-    'Symfony\\Component\\EventDispatcher' => 'phar://Guzzle/vendor/symfony/event-dispatcher',
-    'Doctrine' => 'phar://Guzzle/vendor/doctrine/common/lib',
-    'Monolog' => 'phar://Guzzle/vendor/monolog/monolog/src'
+    'Guzzle' => 'phar://guzzle/src',
+    'Symfony\\Component\\EventDispatcher' => 'phar://guzzle/vendor/symfony/event-dispatcher',
+    'Doctrine' => 'phar://guzzle/vendor/doctrine/common/lib',
+    'Monolog' => 'phar://guzzle/vendor/monolog/monolog/src'
 ));
 $classLoader->register();
 
