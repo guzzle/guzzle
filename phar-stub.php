424e3d555e028f37dc1895272b40589f2f624bdf
Michael Dowling <mtdowling@gmail.com>
2012-05-09 16:19:47 -0700

    Removing alias from phar file

--- phar-stub.php
+++ phar-stub.php
@@ -1,13 +1,15 @@
 <?php
 
-require_once 'phar://guzzle/vendor/symfony/class-loader/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+Phar::mapPhar('guzzle.phar');
+
+require_once 'phar://guzzle.phar/vendor/symfony/class-loader/Symfony/Component/ClassLoader/UniversalClassLoader.php';
 
 $classLoader = new Symfony\Component\ClassLoader\UniversalClassLoader();
 $classLoader->registerNamespaces(array(
-    'Guzzle' => 'phar://guzzle/src',
-    'Symfony\\Component\\EventDispatcher' => 'phar://guzzle/vendor/symfony/event-dispatcher',
-    'Doctrine' => 'phar://guzzle/vendor/doctrine/common/lib',
-    'Monolog' => 'phar://guzzle/vendor/monolog/monolog/src'
+    'Guzzle' => 'phar://guzzle.phar/src',
+    'Symfony\\Component\\EventDispatcher' => 'phar://guzzle.phar/vendor/symfony/event-dispatcher',
+    'Doctrine' => 'phar://guzzle.phar/vendor/doctrine/common/lib',
+    'Monolog' => 'phar://guzzle.phar/vendor/monolog/monolog/src'
 ));
 $classLoader->register();
 
