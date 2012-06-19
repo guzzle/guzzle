0891bdbfbef0119ea69a6d805a7ba7e6542682db
Gordon Franke <info@nevalon.de>
2012-06-19 19:33:48 +0300

    fix exception message

--- src/Guzzle/Common/Validation/AbstractType.php
+++ src/Guzzle/Common/Validation/AbstractType.php
@@ -22,7 +22,7 @@ abstract class AbstractType extends AbstractConstraint
 
         if (!isset(static::$typeMapping[$type])) {
             throw new InvalidArgumentException("{$type} is not one of the "
-                . 'mapped types: ' . implode(', ', array_keys(self::$typeMapping)));
+                . 'mapped types: ' . implode(', ', array_keys(static::$typeMapping)));
         }
 
         $method = static::$typeMapping[$type];
