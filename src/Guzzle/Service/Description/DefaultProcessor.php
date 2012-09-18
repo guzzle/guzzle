9dbd507774c8f96d76984ef5c6db9e5ef69abdc7
Michael Dowling <mtdowling@gmail.com>
2012-09-18 00:17:33 -0700

    Horrible optimizations to the JSON schema validation to limit the number of calls made when validating. Resulted in about a 35% speed improvement of a core feature that could potentially be called hundreds of times when sending complex requests.

--- src/Guzzle/Service/Description/DefaultProcessor.php
+++ src/Guzzle/Service/Description/DefaultProcessor.php
@@ -46,20 +46,29 @@ class DefaultProcessor implements ProcessorInterface
      */
     public function process(Parameter $param, &$value)
     {
-        return $this->recursiveProcess($param, $value);
+        $errors = array();
+        $this->recursiveProcess($param, $value, $errors);
+
+        if (empty($errors)) {
+            return true;
+        } else {
+            sort($errors);
+            return $errors;
+        }
     }
 
     /**
      * Recursively validate a parameter
      *
-     * @param Parameter $param API parameter being validated
-     * @param mixed     $value Value to validate and process. The value may change during this process.
-     * @param string    $path  Current validation path (used for error reporting)
-     * @param int       $depth Current depth in the validation process
+     * @param Parameter $param  API parameter being validated
+     * @param mixed     $value  Value to validate and process. The value may change during this process.
+     * @param array     $errors Validation errors encountered
+     * @param string    $path   Current validation path (used for error reporting)
+     * @param int       $depth  Current depth in the validation process
      *
-     * @return bool|array Returns true if valid, or an array of error messages if invalid
+     * @return bool Returns true if valid, or false if invalid
      */
-    protected function recursiveProcess(Parameter $param, &$value, $path = '', $depth = 0)
+    protected function recursiveProcess(Parameter $param, &$value, array &$errors, $path = '', $depth = 0)
     {
         // Update the value by adding default or static values
         $value = $param->getValue($value);
@@ -69,13 +78,11 @@ class DefaultProcessor implements ProcessorInterface
             return true;
         }
 
-        $errors = array();
+        $type = $param->getType();
         // If a name is set then update the path so that validation messages are more helpful
         if ($name = $param->getName()) {
             $path .= "[{$name}]";
         }
-
-        $type = $param->getType();
 
         if ($type == 'object') {
 
@@ -86,7 +93,7 @@ class DefaultProcessor implements ProcessorInterface
             }
 
             // Determine whether or not this "value" has properties and should be traversed
-            $traverse = $tempSet = false;
+            $traverse = $temporaryValue = false;
             if (is_array($value)) {
                 // Ensure that the array is associative and not numerically indexed
                 if (isset($value[0])) {
@@ -98,36 +105,68 @@ class DefaultProcessor implements ProcessorInterface
                 $traverse = true;
             } elseif ($value === null) {
                 // Attempt to let the contents be built up by default values if possible
-                $tempSet = true;
+                $temporaryValue = true;
                 $value = array();
                 $traverse = true;
             }
 
             if ($traverse) {
+
                 if ($properties = $param->getProperties()) {
                     // if properties were found, the validate each property of the value
                     foreach ($properties as $property) {
-                        $this->validateProperty($property, $value, $path, $depth, $errors);
+                        $name = $property->getName();
+                        $current = isset($value[$name]) ? $value[$name] : null;
+                        if ($this->recursiveProcess($property, $current, $errors, $path, $depth + 1)) {
+                            // Only set the value if it was populated with something
+                            if ($current) {
+                                $value[$name] = $current;
+                            }
+                        }
                     }
                 }
+
                 $additional = $param->getAdditionalProperties();
                 if ($additional !== true) {
                     // If additional properties were found, then validate each against the additionalProperties attr.
-                    $this->validateAdditionalProperties($additional, $properties, $value, $errors, $path, $depth);
-                }
-            }
+                    if (is_array($value)) {
+                        $keys = array_keys($value);
+                    } else {
+                        $keys = array();
+                        foreach ($value as $k => $v) {
+                            $keys[] = $k;
+                        }
+                    }
 
-            if ($tempSet && empty($value)) {
-                $value = null;
+                    // Determine the keys that were specified that were not listed in the properties of the schema
+                    $diff = array_diff($keys, array_keys($properties));
+                    if (!empty($diff)) {
+                        // Determine which keys are not in the properties
+                        if ($additional instanceOf Parameter) {
+                            foreach ($diff as $key) {
+                                $v = $value[$key];
+                                $this->recursiveProcess($additional, $v, $errors, "{$path}[{$key}]", $depth);
+                            }
+                        } else {
+                            // if additionalProperties is set to false and there are additionalProperties in the values, then fail
+                            $keys = array_keys($value);
+                            $errors[] = sprintf('%s[%s] is not an allowed property', $path, reset($keys));
+                        }
+                    }
+                }
+
+                // A temporary value will be used to traverse elements that have no corresponding input value.
+                // This allows nested required parameters with defautl values to bubble up into the input.
+                // Here we check if we used a temp value and nothing bubbled up, then we need to remote the value.
+                if ($temporaryValue && empty($value)) {
+                    $value = null;
+                }
             }
 
         } elseif ($type == 'array' && $param->getItems() && is_array($value)) {
             foreach ($value as $i => &$item) {
                 // Validate each item in an array against the items attribute of the schema
-                $e = $this->recursiveProcess($param->getItems(), $item, $path . "[{$i}]", $depth + 1);
-                if ($e !== true) {
-                    $errors = array_merge($errors, $e);
-                }
+                $this->recursiveProcess($param->getItems(), $item, $errors, $path . "[{$i}]", $depth + 1);
             }
         }
 
@@ -138,11 +177,12 @@ class DefaultProcessor implements ProcessorInterface
                 $message .= ': ' . $param->getDescription();
             }
             $errors[] = $message;
+
         } else {
 
             // Validate that the type is correct. If the type is string but an integer was passed, the class can be
             // instructed to cast the integer to a string to pass validation. This is the default behavior.
-            if ($type && (!$type = $this->determineType($param, $value))) {
+            if ($type && (!$type = $this->determineType($param->getType(), $value))) {
                 if ($this->castIntegerToStringType && $param->getType() == 'string' && is_integer($value)) {
                     $value = (string) $value;
                 } else {
@@ -189,130 +229,65 @@ class DefaultProcessor implements ProcessorInterface
 
         // Determine what the response should be
         if (empty($errors)) {
-            // If no errors were found, then filter the value and return true
             $value = $param->filter($value);
             return true;
-        } elseif ($depth == 0) {
-            // If errors were found and this is the outer recursive function, then return a sorted list of errors
-            sort($errors);
-            return $errors;
         } else {
-            // If errors were found in a recursive call, then return them. They will be merged in to the parent scope.
-            return $errors;
-        }
-    }
-
-    /**
-     * Validate additional properties. If set to false and there are properties specified that are not specifically
-     * specified in the schema, then fail. If set to a schema, then validate all additional properties against it.
-     *
-     * @param Parameter|bool $additional Additional properties
-     * @param array          $properties Allowable properties
-     * @param mixed          $value      Value being validated
-     * @param array          $errors     Errors collection
-     * @param string         $path       Current validation path
-     * @param int            $depth      Current validation depth
-     */
-    protected function validateAdditionalProperties($additional, array $properties, &$value, &$errors, $path, $depth)
-    {
-        if (is_array($value)) {
-            $keys = array_keys($value);
-        } else {
-            foreach ($value as $k => $v) {
-                $keys[] = $k;
-            }
-        }
-
-        // Determine the keys that were specified that were not listed in the properties of the schema
-        $diff = array_diff($keys, array_keys($properties));
-
-        if (!empty($diff)) {
-            // Determine which keys are not in the properties
-            if ($additional instanceOf Parameter) {
-                foreach ($diff as $key) {
-                    $v = &$value[$key];
-                    $e = $this->recursiveProcess($additional, $v, "{$path}[{$key}]", $depth);
-                    if ($e !== true) {
-                        $errors = array_merge($errors, $e);
-                    }
-                }
-            } else {
-                // if additionalProperties is set to false and there are additionalProperties in the values, then fail
-                $keys = array_keys($value);
-                $errors[] = sprintf('%s[%s] is not an allowed property', $path, reset($keys));
-            }
-        }
-    }
-
-    /**
-     * Process a property
-     *
-     * @param Parameter $property API parameter property
-     * @param mixed     $value    Value to process
-     * @param string    $path     Current validation path
-     * @param int       $depth    Current validation depth
-     * @param array     $errors   Errors encountered
-     */
-    protected function validateProperty(Parameter $property, &$value, $path, $depth, array &$errors)
-    {
-        $name = $property->getName();
-        $current = isset($value[$name]) ? $value[$name] : null;
-        $e = $this->recursiveProcess($property, $current, $path, $depth + 1);
-        if ($e !== true) {
-            $errors = array_merge($errors, $e);
-        } elseif ($current) {
-            $value[$name] = $current;
+            return false;
         }
     }
 
     /**
      * From the allowable types, determine the type that the variable matches
      *
-     * @param Parameter $param Parameter that is being validated
-     * @param mixed     $value Value to determine the type
+     * @param string $type  Parameter type
+     * @param mixed  $value Value to determine the type
      *
-     * @return string|bool
+     * @return string|bool Returns the matching type on
      */
-    protected function determineType(Parameter $param, $value)
+    protected function determineType($type, $value)
     {
-        foreach ((array) $param->getType() as $type) {
-            if ($this->checkType($type, $value)) {
-                return $type;
+        foreach ((array) $type as $t) {
+            switch ($t) {
+                case 'string':
+                    if (is_string($value) || (is_object($value) && method_exists($value, '__toString'))) {
+                        return 'string';
+                    }
+                    break;
+                case 'integer':
+                    if (is_integer($value)) {
+                        return 'integer';
+                    }
+                    break;
+                case 'numeric':
+                    if (is_numeric($value)) {
+                        return 'numeric';
+                    }
+                    break;
+                case 'object':
+                    if (is_array($value) || is_object($value)) {
+                        return 'object';
+                    }
+                    break;
+                case 'array':
+                    if (is_array($value)) {
+                        return 'array';
+                    }
+                    break;
+                case 'boolean':
+                    if (is_bool($value)) {
+                        return 'boolean';
+                    }
+                    break;
+                case 'null':
+                    if (!$value) {
+                        return 'null';
+                    }
+                    break;
+                case 'any':
+                    return 'any';
             }
         }
 
         return false;
-    }
-
-    /**
-     * Check if a value is a particular type
-     *
-     * @param string $type  Type to check
-     * @param string $value Value to check
-     *
-     * @return bool
-     */
-    protected function checkType($type, $value)
-    {
-        if ($type && $type != 'any') {
-            switch ($type) {
-                case 'string':
-                    return is_string($value) || (is_object($value) && method_exists($value, '__toString'));
-                case 'integer':
-                    return is_integer($value);
-                case 'numeric':
-                    return is_numeric($value);
-                case 'object':
-                    return is_array($value) || is_object($value);
-                case 'array':
-                    return is_array($value);
-                case 'boolean':
-                    return is_bool($value);
-                case 'null':
-                    return !$value;
-            }
-        }
-
-        return true;
     }
 }
