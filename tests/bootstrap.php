d22230f1b610ca3d95058a119376819fa0946e1d
Michael Dowling <mtdowling@gmail.com>
2012-10-31 21:27:06 -0700

    Adding phpunit as dev dep so that it is easier for new users to get up and running

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -16,7 +16,7 @@ if (get_cfg_var('guzzle_phar')) {
 }
 
 // Include the composer autoloader
-$autoloader = require_once dirname(__DIR__) . '/vendor/autoload.php';
+$autoloader = require dirname(__DIR__) . '/vendor/autoload.php';
 
 // Add the services file to the default service builder
 $servicesFile = __DIR__ . '/Guzzle/Tests/TestData/services/services.json';
