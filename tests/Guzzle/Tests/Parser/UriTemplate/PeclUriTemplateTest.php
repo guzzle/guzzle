41de374600bada4ac6e75d27da5207201640aa8b
Adrien Brault <adrien.brault@gmail.com>
2012-11-16 16:25:30 +0100

    Skip tests that need the pecl uri_template lib when it's not installed

--- tests/Guzzle/Tests/Parser/UriTemplate/PeclUriTemplateTest.php
+++ tests/Guzzle/Tests/Parser/UriTemplate/PeclUriTemplateTest.php
@@ -9,6 +9,13 @@ use Guzzle\Parser\UriTemplate\PeclUriTemplate;
  */
 class PeclUriTemplateTest extends AbstractUriTemplateTest
 {
+    protected function setUp()
+    {
+        if (!extension_loaded('uri_template')) {
+            $this->markTestSkipped('uri_template PECL extension must be installed to test PeclUriTemplate');
+        }
+    }
+
     /**
      * @dataProvider templateProvider
      */
