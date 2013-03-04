61d86f9384b18536a39c13db9d47a289a22b8cde
Michael Dowling <mtdowling@gmail.com>
2013-03-03 20:42:34 -0800

    Adding the ability to specify stream context parameters

--- src/Guzzle/Stream/StreamRequestFactoryInterface.php
+++ src/Guzzle/Stream/StreamRequestFactoryInterface.php
@@ -12,11 +12,12 @@ interface StreamRequestFactoryInterface
     /**
      * Create a stream based on a request object
      *
-     * @param RequestInterface $request        Base the stream on a request
-     * @param array            $contextOptions Custom context options to merge into the default context options
+     * @param RequestInterface $request Base the stream on a request
+     * @param array            $options Custom context options to merge into the default context options
+     * @param array            $params  Custom context params to use with the request
      *
      * @return resource Returns a stream resource that can be used like fopen resources
      * @throws \Guzzle\Common\Exception\RuntimeException if the stream cannot be opened or an error occurs
      */
-    public function fromRequest(RequestInterface $request, array $contextOptions = null);
+    public function fromRequest(RequestInterface $request, array $options = null, array $params = array());
 }
