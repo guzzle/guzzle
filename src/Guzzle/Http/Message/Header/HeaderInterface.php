5c1e813427c2ec6db86133ce683b108791e01f87
Michael Dowling <mtdowling@gmail.com>
2013-05-29 09:21:10 -0700

    Correcting docblock

--- src/Guzzle/Http/Message/Header/HeaderInterface.php
+++ src/Guzzle/Http/Message/Header/HeaderInterface.php
@@ -83,7 +83,7 @@ interface HeaderInterface extends ToArrayInterface, \Countable, \IteratorAggrega
     public function removeValue($searchValue);
 
     /**
-     * Convert a header containing ";" separated data into an array of associative arrays representing the header
+     * Parse a header containing ";" separated data into an array of associative arrays representing the header
      * key value pair data of the header. When a parameter does not contain a value, but just contains a key, this
      * function will inject a key with a '' string value.
      *
