a97fc9a9f826eb2075b8816b62b83457d9bd8fa5
Michael Dowling <mtdowling@gmail.com>
2013-05-28 20:00:19 -0700

    Removing unused statements from bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -21,9 +21,3 @@ $autoloader = require dirname(__DIR__) . '/vendor/autoload.php';
 // Add the services file to the default service builder
 $servicesFile = __DIR__ . '/Guzzle/Tests/TestData/services/services.json';
 Guzzle\Tests\GuzzleTestCase::setServiceBuilder(Guzzle\Service\Builder\ServiceBuilder::factory($servicesFile));
-
-// Modify the include path so that it can find the Zend Framework
-$paths = array('vendor/zend/zend-cache1', 'vendor/zend/zend-log1');
-set_include_path(implode(PATH_SEPARATOR, array_map(function($path) {
-    return __DIR__ . '/../' . $path;
-}, $paths)) . PATH_SEPARATOR . get_include_path());
