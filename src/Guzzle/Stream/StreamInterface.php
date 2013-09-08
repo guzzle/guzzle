524118a94835127374c0e7f7737c8d526f694320
Michael Dowling <mtdowling@gmail.com>
2013-09-08 15:06:08 -0700

    Better organized StreamInterface

--- src/Guzzle/Stream/StreamInterface.php
+++ src/Guzzle/Stream/StreamInterface.php
@@ -8,6 +8,16 @@ namespace Guzzle\Stream;
 interface StreamInterface
 {
     /**
+     * Convert the stream to a string by calling read until the end of the
+     * stream is reached.
+     *
+     * Warning: This will attempt to load the entire stream into memory.
+     *
+     * @return string
+     */
+    public function __toString();
+
+    /**
      * Close the stream
      */
     public function close();
@@ -34,6 +44,13 @@ interface StreamInterface
     public function tell();
 
     /**
+     * Returns true if the stream is at the end of the stream.
+     *
+     * @return bool
+     */
+    public function eof();
+
+    /**
      * Returns whether or not the stream is seekable
      *
      * @return bool
@@ -50,6 +67,42 @@ interface StreamInterface
      * @link   http://www.php.net/manual/en/function.fseek.php
      */
     public function seek($offset, $whence = SEEK_SET);
+
+    /**
+     * Returns whether or not the stream is writable
+     *
+     * @return bool
+     */
+    public function isWritable();
+
+    /**
+     * Write data to the stream
+     *
+     * @param string $string The string that is to be written.
+     *
+     * @return int|bool Returns the number of bytes written to the stream on
+     *                  success or FALSE on failure.
+     */
+    public function write($string);
+
+    /**
+     * Returns whether or not the stream is readable
+     *
+     * @return bool
+     */
+    public function isReadable();
+
+    /**
+     * Read data from the stream
+     *
+     * @param int $length Read up to $length bytes from the object and return
+     *                    them. Fewer than $length bytes may be returned if
+     *                    underlying stream call returns fewer bytes.
+     *
+     * @return string|bool Returns the data read from the stream or false on
+     *                     failure or when the end of the stream is reached.
+     */
+    public function read($length);
 
     /**
      * Get stream metadata as an associative array or retrieve a specific key.
@@ -81,57 +134,4 @@ interface StreamInterface
      * @see http://php.net/manual/en/function.stream-get-meta-data.php
      */
     public function getMetadata($key = null);
-
-    /**
-     * Returns whether or not the stream is writable
-     *
-     * @return bool
-     */
-    public function isWritable();
-
-    /**
-     * Write data to the stream
-     *
-     * @param string $string The string that is to be written.
-     *
-     * @return int|bool Returns the number of bytes written to the stream on
-     *                  success or FALSE on failure.
-     */
-    public function write($string);
-
-    /**
-     * Convert the stream to a string by calling read until the end of the
-     * stream is reached.
-     *
-     * Warning: This will attempt to load the entire stream into memory.
-     *
-     * @return string
-     */
-    public function __toString();
-
-    /**
-     * Returns whether or not the stream is readable
-     *
-     * @return bool
-     */
-    public function isReadable();
-
-    /**
-     * Read data from the stream
-     *
-     * @param int $length Read up to $length bytes from the object and return
-     *                    them. Fewer than $length bytes may be returned if
-     *                    underlying stream call returns fewer bytes.
-     *
-     * @return string|bool Returns the data read from the stream or false on
-     *                     failure or when the end of the stream is reached.
-     */
-    public function read($length);
-
-    /**
-     * Returns true if the stream is at the end of the stream.
-     *
-     * @return bool
-     */
-    public function eof();
 }
