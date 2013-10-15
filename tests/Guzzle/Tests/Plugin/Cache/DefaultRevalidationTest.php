4ceb7fe88836b8736e5039771000351e04605676
Artur Bodera <abodera@gmail.com>
2013-10-16 00:46:54 +0200

    Add tests.

--- tests/Guzzle/Tests/Plugin/Cache/DefaultRevalidationTest.php
+++ tests/Guzzle/Tests/Plugin/Cache/DefaultRevalidationTest.php
@@ -211,4 +211,36 @@ class DefaultRevalidationTest extends \Guzzle\Tests\GuzzleTestCase
         $this->assertEquals('HIT from GuzzleCache', (string) $response->getHeader('X-Cache-Lookup'));
         $this->assertEquals('HIT_ERROR from GuzzleCache', (string) $response->getHeader('X-Cache'));
     }
+
+    /**
+     * @group issue-437
+     */
+    public function testDoesNotTouchClosureListeners()
+    {
+        $this->getServer()->flush();
+        $this->getServer()->enqueue(array(
+            "HTTP/1.1 200 OK\r\n" .
+            "Date: Mon, 12 Nov 2012 03:06:37 GMT\r\n" .
+            "Cache-Control: private, s-maxage=0, max-age=0, must-revalidate\r\n" .
+            "Last-Modified: Mon, 12 Nov 2012 02:53:38 GMT\r\n" .
+            "Content-Length: 2\r\n\r\nhi",
+            "HTTP/1.0 304 Not Modified\r\n" .
+            "Date: Mon, 12 Nov 2012 03:06:38 GMT\r\n" .
+            "Content-Type: text/html; charset=UTF-8\r\n" .
+            "Last-Modified: Mon, 12 Nov 2012 02:53:38 GMT\r\n" .
+            "Age: 6302\r\n\r\n",
+            "HTTP/1.0 304 Not Modified\r\n" .
+            "Date: Mon, 12 Nov 2012 03:06:38 GMT\r\n" .
+            "Content-Type: text/html; charset=UTF-8\r\n" .
+            "Last-Modified: Mon, 12 Nov 2012 02:53:38 GMT\r\n" .
+            "Age: 6302\r\n\r\n",
+        ));
+        $client = new Client($this->getServer()->getUrl());
+        $client->addSubscriber(new CachePlugin());
+        $client->getEventDispatcher()->addListener('command.after_send', function(){});
+        $this->assertEquals(200, $client->get()->send()->getStatusCode());
+        $this->assertEquals(200, $client->get()->send()->getStatusCode());
+        $this->assertEquals(200, $client->get()->send()->getStatusCode());
+    }
+
 }
