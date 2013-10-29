ece5342209a9f5b3feadd77c1b142ebc56a9b5bb
Michael Dowling <mtdowling@gmail.com>
2013-10-28 22:38:13 -0700

    Cleaning up build stuff for a leaner release process

--- phar-stub.php
+++ phar-stub.php
@@ -7,9 +7,7 @@ require_once 'phar://guzzle.phar/vendor/symfony/class-loader/Symfony/Component/C
 $classLoader = new Symfony\Component\ClassLoader\UniversalClassLoader();
 $classLoader->registerNamespaces(array(
     'Guzzle' => 'phar://guzzle.phar/src',
-    'Symfony\\Component\\EventDispatcher' => 'phar://guzzle.phar/vendor/symfony/event-dispatcher',
-    'Doctrine' => 'phar://guzzle.phar/vendor/doctrine/common/lib',
-    'Monolog' => 'phar://guzzle.phar/vendor/monolog/monolog/src'
+    'Symfony\\Component\\EventDispatcher' => 'phar://guzzle.phar/vendor/symfony/event-dispatcher'
 ));
 $classLoader->register();
 
