a280594ffbb1b563a0f042182221f39e7de9efa3
Michael Dowling <mtdowling@gmail.com>
2013-11-02 14:40:25 -0700

    Cleaning up phar, test bootstrap, and loading build.properties.dist by default.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,21 +2,8 @@
 
 error_reporting(E_ALL | E_STRICT);
 
-// Ensure that composer has installed all dependencies
-if (!file_exists(dirname(__DIR__) . '/composer.lock')) {
-    die("Dependencies must be installed using composer:\n\nphp composer.phar install --dev\n\n"
-        . "See http://getcomposer.org for help with installing composer\n");
-}
-
 require_once 'PHPUnit/TextUI/TestRunner.php';
-
-// Include the phar files if testing against the phars
-if (get_cfg_var('guzzle_phar')) {
-    require get_cfg_var('guzzle_phar');
-}
-
-// Include the composer autoloader
-$autoloader = require dirname(__DIR__) . '/vendor/autoload.php';
+require dirname(__DIR__) . '/vendor/autoload.php';
 
 // Add the services file to the default service builder
 $servicesFile = __DIR__ . '/Guzzle/Tests/TestData/services/services.json';
