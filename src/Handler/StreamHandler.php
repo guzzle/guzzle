7a8a6c6d5ca885f28733c8ba6af57ab55d992ae8
hw <hussainweb@gmail.com>
2015-06-25 11:09:41 +0530

    Fix failure with HHVM in StreamHandlerTest::testAddsErrorToResponse().

--- src/Handler/StreamHandler.php
+++ src/Handler/StreamHandler.php
@@ -45,6 +45,7 @@ class StreamHandler
             // This list can probably get more comprehensive.
             if (strpos($message, 'getaddrinfo') // DNS lookup failed
                 || strpos($message, 'Connection refused')
+                || strpos($message, "couldn't connect to host")
             ) {
                 $e = new ConnectException($e->getMessage(), $request, $e);
             }
