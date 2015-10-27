3937279ba1af3f0c1f980073814e3573d3d1618b
Dale Attree <dale.attree@gmail.com>
2015-10-27 14:45:27 +0200

    Update StreamHandlerTest.php
    Incorrect assertion added

--- tests/Handler/StreamHandlerTest.php
+++ tests/Handler/StreamHandlerTest.php
@@ -245,7 +245,6 @@ class StreamHandlerTest extends \PHPUnit_Framework_TestCase
         $res = $this->getSendResult(['verify' => $path]);
         $opts = stream_context_get_options($res->getBody()->detach());
         $this->assertEquals(true, $opts['ssl']['verify_peer']);
-        $this->assertEquals(true, $opts['ssl']['verify_peer_name']);
         $this->assertEquals($path, $opts['ssl']['cafile']);
         $this->assertTrue(file_exists($opts['ssl']['cafile']));
     }
