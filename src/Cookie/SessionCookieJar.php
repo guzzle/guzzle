c0c8969ca0220b2f1f0bd58edba45a5d245b0fa2
Rafał Osiecki <metempsychoza@wp.pl>
2016-06-03 00:45:36 +0200

    PHP 7 fix

--- src/Cookie/SessionCookieJar.php
+++ src/Cookie/SessionCookieJar.php
@@ -56,11 +56,10 @@ class SessionCookieJar extends CookieJar
      */
     protected function load()
     {
-        $cookieJar = isset($_SESSION[$this->sessionKey])
-            ? $_SESSION[$this->sessionKey]
-            : null;
-
-        $data = json_decode($cookieJar, true);
+        if (!isset($_SESSION[$this->sessionKey])) {
+            return;
+        }
+        $data = json_decode($_SESSION[$this->sessionKey], true);
         if (is_array($data)) {
             foreach ($data as $cookie) {
                 $this->setCookie(new SetCookie($cookie));
