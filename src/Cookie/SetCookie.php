a53a9cfe37c8f20eb77c3f1ac238c5aa9bfc7f58
elo <eloindev@gmail.com>
2017-08-09 11:18:47 +0900

    Fix an error due to index 0

--- src/Cookie/SetCookie.php
+++ src/Cookie/SetCookie.php
@@ -35,8 +35,9 @@ class SetCookie
         $data = self::$defaults;
         // Explode the cookie string using a series of semicolons
         $pieces = array_filter(array_map('trim', explode(';', $cookie)));
+        $pieces = array_values($pieces);
         // The name of the cookie (first kvp) must include an equal sign.
-        if (empty($pieces) || (isset($pieces[0]) && !strpos($pieces[0], '='))) {
+        if (empty($pieces) || !strpos($pieces[0], '=')) {
             return new self($data);
         }
 
