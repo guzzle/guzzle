264fa9a49870912aeca75460960ee80af8a77dfe
iorsa <32541698+iorsa@users.noreply.github.com>
2017-10-05 16:36:26 +0500

    Update CurlMultiHandler.php
    Do-while mistake that can cause problems with curl on some systems.
    Am I correct or that's fine?

--- src/Handler/CurlMultiHandler.php
+++ src/Handler/CurlMultiHandler.php
@@ -95,15 +95,15 @@ class CurlMultiHandler
         // Step through the task queue which may add additional requests.
         P\queue()->run();
 
-        if ($this->active &&
-            curl_multi_select($this->_mh, $this->selectTimeout) === -1
-        ) {
-            // Perform a usleep if a select returns -1.
-            // See: https://bugs.php.net/bug.php?id=61141
-            usleep(250);
-        }
-
-        while (curl_multi_exec($this->_mh, $this->active) === CURLM_CALL_MULTI_PERFORM);
+        do {
+            if ($this->active &&
+                curl_multi_select($this->_mh, $this->selectTimeout) === -1
+            ) {
+                // Perform a usleep if a select returns -1.
+                // See: https://bugs.php.net/bug.php?id=61141
+                usleep(250);
+            }
+        } while (curl_multi_exec($this->_mh, $this->active) === CURLM_CALL_MULTI_PERFORM);
 
         $this->processMessages();
     }
