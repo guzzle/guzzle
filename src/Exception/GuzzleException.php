239993a013e07e41da5034d6eb3aef2c9c9a1d34
Richard van Laak <rvanlaak@gmail.com>
2018-06-26 13:31:02 +0200

    #1971 Allow PHPStan to match with `@throws` phpdoc

--- src/Exception/GuzzleException.php
+++ src/Exception/GuzzleException.php
@@ -1,13 +1,4 @@
 <?php
 namespace GuzzleHttp\Exception;
 
-/**
- * @method string getMessage()
- * @method \Throwable|null getPrevious()
- * @method mixed getCode()
- * @method string getFile()
- * @method int getLine()
- * @method array getTrace()
- * @method string getTraceAsString()
- */
-interface GuzzleException {}
+interface GuzzleException extends \Throwable {}
