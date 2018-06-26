9fc8c3ad4a47540491137c6f5601e3445b7f5287
Frost Wong <frostwong@gmail.com>
2018-06-26 23:17:39 +0800

    Add support for Restful APIs

---
+++ src/RestfulApiMiddleware.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Frost Wong <frostwong@gmail.com>
+ * Date: 2018/6/26
+ * Time: 22:59
+ */
+
+namespace GuzzleHttp;
+
+
+use GuzzleHttp\Psr7\Uri;
+use Psr\Http\Message\RequestInterface;
+
+
+/**
+ * Pre-process URIs which contains attributes with values provided by $options['attributes']
+ */
+class RestfulApiMiddleware
+{
+
+    private $nextHandler;
+
+    public function __construct(callable $nextHandler)
+    {
+        $this->nextHandler = $nextHandler;
+    }
+
+    public function __invoke(RequestInterface $request, array $options)
+    {
+        $fn = $this->nextHandler;
+
+        // Don't do any thing if attributes is not set
+        if (!isset($options['attributes'])) {
+            return $fn($request, $options);
+        }
+
+        $attributes = $options['attributes'];
+        $uri = (string)$request->getUri();
+
+        $translatedUri = $this->interpolate($uri, $attributes);
+
+        $request = $request->withUri(new Uri($translatedUri), true);
+
+        return $fn($request, $options);
+    }
+
+    /**
+     * Translate placeholders with context.
+     *
+     * @param $message
+     * @param array $context
+     * @return string
+     */
+    private function interpolate($message, array $context = [])
+    {
+        $placeholders = [];
+
+        foreach ($context as $key => $val) {
+            if (! is_array($val) && (! is_object($val) || method_exists($val, '__toString'))) {
+                $placeholders['{' . $key . '}'] = $val;
+            }
+        }
+
+        return strtr(urldecode($message), $placeholders);
+    }
+}
