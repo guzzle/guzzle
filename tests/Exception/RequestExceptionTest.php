5c7a5c524e556113e7f8685e6880cbc4090c798d
SpacePossum <possumfromspace@gmail.com>
2018-08-08 06:37:00 +0200

    RequestException - check if readable before access (#2081)
    * check if readable before access
    
    * Add unit tests for RequestException::getResponseBodySummary
    
    * use real object in test
    
    * update test for older PHP version support
    
    * remove no longer used imports
    
    * Update RequestExceptionTest.php

--- tests/Exception/RequestExceptionTest.php
+++ tests/Exception/RequestExceptionTest.php
@@ -4,6 +4,7 @@ namespace GuzzleHttp\Tests\Event;
 use GuzzleHttp\Exception\RequestException;
 use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Psr7\Stream;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -173,5 +174,28 @@ class RequestExceptionTest extends TestCase
         $r = new Request('GET', 'http://user:password@www.oo.com');
         $e = RequestException::create($r, new Response(500));
         $this->assertContains('http://user:***@www.oo.com', $e->getMessage());
+    }
+
+    public function testGetResponseBodySummaryOfNonReadableStream()
+    {
+        $this->assertNull(RequestException::getResponseBodySummary(new Response(500, [], new ReadSeekOnlyStream())));
+    }
+}
+
+final class ReadSeekOnlyStream extends Stream
+{
+    public function __construct()
+    {
+        parent::__construct(fopen('php://memory', 'wb'));
+    }
+
+    public function isSeekable()
+    {
+        return true;
+    }
+
+    public function isReadable()
+    {
+        return false;
     }
 }
