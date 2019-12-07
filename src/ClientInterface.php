a9df666fcd3015cb123a387653eef92293448937
Nyholm <tobias.nyholm@gmail.com>
2019-12-07 10:29:45 +0100

    Prepare version 6.5

--- src/ClientInterface.php
+++ src/ClientInterface.php
@@ -15,7 +15,7 @@ interface ClientInterface
     /**
      * @deprecated Will be removed in Guzzle 7.0.0
      */
-    const VERSION = '6.4.1';
+    const VERSION = '6.5.0';
 
     /**
      * Send an HTTP request.
