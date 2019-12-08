6800c91fe7a9c7f5b4386c5650250fd35d9ea12a
Mponos George <gmponos@gmail.com>
2019-12-08 17:01:12 +0200

    Disallow empty response on BadResponseException (#2427)
    * Disallow empty response on BadResponseException
    
    * Fix codestyle
    
    * Added return type
    
    * Added typehint
    
    * Remove error from phpstan baseline

--- src/Exception/BadResponseException.php
+++ src/Exception/BadResponseException.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace GuzzleHttp\Exception;
 
 use Psr\Http\Message\RequestInterface;
@@ -6,22 +7,23 @@ use Psr\Http\Message\ResponseInterface;
 
 /**
  * Exception when an HTTP error occurs (4xx or 5xx error)
+ *
+ * @method ResponseInterface getResponse()
  */
 class BadResponseException extends RequestException
 {
     public function __construct(
-        $message,
+        string $message,
         RequestInterface $request,
-        ResponseInterface $response = null,
+        ResponseInterface $response,
         \Exception $previous = null,
         array $handlerContext = []
     ) {
-        if (null === $response) {
-            @trigger_error(
-                'Instantiating the ' . __CLASS__ . ' class without a Response is deprecated since version 6.3 and will be removed in 7.0.',
-                E_USER_DEPRECATED
-            );
-        }
         parent::__construct($message, $request, $response, $previous, $handlerContext);
+    }
+
+    public function hasResponse(): bool
+    {
+        return true;
     }
 }
