46415a7f19ff6e7a42be44f9ce455dee63a7cd7d
Saša Stamenković <umpirsky@gmail.com>
2020-04-28 17:56:31 +0200

    Respect logging level in Middleware

--- src/Middleware.php
+++ src/Middleware.php
@@ -198,7 +198,7 @@ final class Middleware
                             ? $reason->getResponse()
                             : null;
                         $message = $formatter->format($request, $response, \GuzzleHttp\Promise\exception_for($reason));
-                        $logger->notice($message);
+                        $logger->log($logLevel, $message);
                         return \GuzzleHttp\Promise\rejection_for($reason);
                     }
                 );
