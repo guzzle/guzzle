a333a79f79a653df03f10a162453d152101b576e
Saša Stamenković <umpirsky@gmail.com>
2020-05-26 09:25:17 +0200

    Use error on exceptions.

--- src/Middleware.php
+++ src/Middleware.php
@@ -198,7 +198,7 @@ final class Middleware
                             ? $reason->getResponse()
                             : null;
                         $message = $formatter->format($request, $response, \GuzzleHttp\Promise\exception_for($reason));
-                        $logger->log($logLevel, $message);
+                        $logger->error($message);
                         return \GuzzleHttp\Promise\rejection_for($reason);
                     }
                 );
