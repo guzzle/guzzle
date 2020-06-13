9337015fff71bbda4f0884c94ca2bdf9fc3b6c86
David Grayston <davidgrayston@googlemail.com>
2020-06-13 02:08:26 +0100

    Check for response before creating RequestException

--- src/Handler/CurlFactory.php
+++ src/Handler/CurlFactory.php
@@ -174,7 +174,7 @@ class CurlFactory implements CurlFactoryInterface
 
         // If an exception was encountered during the onHeaders event, then
         // return a rejected promise that wraps that exception.
-        if ($easy->onHeadersException) {
+        if ($easy->response && $easy->onHeadersException) {
             return \GuzzleHttp\Promise\rejection_for(
                 new RequestException(
                     'An error was encountered during the on_headers event',
