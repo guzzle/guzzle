4f5c114bd11ffd78dd2e7569ae48171013122b73
Graham Campbell <GrahamCampbell@users.noreply.github.com>
2020-06-27 09:52:49 +0100

    Deprecate Client::__call (#2557)

--- src/Client.php
+++ src/Client.php
@@ -71,6 +71,8 @@ class Client implements ClientInterface, \Psr\Http\Client\ClientInterface
      * @param array  $args
      *
      * @return PromiseInterface|ResponseInterface
+     *
+     * @deprecated Client::__call will be removed in guzzlehttp/guzzle:8.0.
      */
     public function __call($method, $args)
     {
