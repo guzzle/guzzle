977b0deb89b843ef88c48444c47bf11390eb9c60
Tobias Nyholm <tobias.nyholm@gmail.com>
2020-09-30 14:35:04 +0200

    Deprecate GUZZLE_CURL_SELECT_TIMEOUT (#2786)

--- src/Handler/CurlMultiHandler.php
+++ src/Handler/CurlMultiHandler.php
@@ -71,6 +71,7 @@ class CurlMultiHandler
         if (isset($options['select_timeout'])) {
             $this->selectTimeout = $options['select_timeout'];
         } elseif ($selectTimeout = Utils::getenv('GUZZLE_CURL_SELECT_TIMEOUT')) {
+            @trigger_error('Since guzzlehttp/guzzle 7.2.0: Using environment variable GUZZLE_CURL_SELECT_TIMEOUT is deprecated. Use option "select_timeout" instead.', \E_USER_DEPRECATED);
             $this->selectTimeout = (int) $selectTimeout;
         } else {
             $this->selectTimeout = 1;
