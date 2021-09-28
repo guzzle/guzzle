209650c769fbc078fe3efed559393aeb1451f7c2
Tim Düsterhus <duesterhus@woltlab.com>
2021-09-28 14:14:31 +0200

    Fix progress parameter type compatibility between cURL and StreamHandler
    Unknown values of the progress called are filled with `0` by cURL and in fact
    the `CURLOPT_PROGRESSFUNCTION` parameters are explicitly typed `int` in
    CurlFactory.
    
    StreamHandler always provided `null` as the upload progress, because the
    progress is not known there. Adjust this to `0` for consistent behavior.

--- src/Handler/StreamHandler.php
+++ src/Handler/StreamHandler.php
@@ -517,7 +517,9 @@ class StreamHandler
             $params,
             static function ($code, $a, $b, $c, $transferred, $total) use ($value) {
                 if ($code == \STREAM_NOTIFY_PROGRESS) {
-                    $value($total, $transferred, null, null);
+                    // The upload progress cannot be determined. Use 0 for cURL compatibility:
+                    // https://curl.se/libcurl/c/CURLOPT_PROGRESSFUNCTION.html
+                    $value($total, $transferred, 0, 0);
                 }
             }
         );
