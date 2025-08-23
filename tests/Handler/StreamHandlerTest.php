4deefcc80749b204dfd5d1eaff38d1fc1f006e5d
Graham Campbell <hello@gjcampbell.co.uk>
2025-08-23 22:58:34 +0100

    Fixes

--- tests/Handler/StreamHandlerTest.php
+++ tests/Handler/StreamHandlerTest.php
@@ -770,11 +770,43 @@ class StreamHandlerTest extends TestCase
         self::assertFalse(\feof($body));
     }
 
-    public function testHandlesGarbageHttpServerGracefully()
+    private static function shouldRunOnThisPhpVersion(): bool
     {
+        return (PHP_VERSION_ID >= 80132 && PHP_VERSION_ID < 80200)
+            || (PHP_VERSION_ID >= 80228 && PHP_VERSION_ID < 80300)
+            || (PHP_VERSION_ID >= 80319 && PHP_VERSION_ID < 80400)
+            || PHP_VERSION_ID >= 80405;
+    }
+
+    public function testHandlesGarbageHttpServerGracefullyLegacy()
+    {
+        if (self::shouldRunOnThisPhpVersion()) {
+            $this->markTestSkipped('This test is not relevant for '.PHP_VERSION);
+        }
+
         $handler = new StreamHandler();
 
         $this->expectException(RequestException::class);
+        $this->expectExceptionMessage('An error was encountered while creating the response');
+
+        $handler(
+            new Request('GET', Server::$url.'guzzle-server/garbage'),
+            [
+                RequestOptions::STREAM => true,
+            ]
+        )->wait();
+    }
+
+    public function testHandlesGarbageHttpServerGracefully()
+    {
+        if (!self::shouldRunOnThisPhpVersion()) {
+            $this->markTestSkipped('This test is not relevant for '.PHP_VERSION);
+        }
+
+        $handler = new StreamHandler();
+
+        $this->expectException(ConnectException::class);
+        $this->expectExceptionMessage('Connection refused for URI '.Server::$url);
 
         $handler(
             new Request('GET', Server::$url.'guzzle-server/garbage'),
