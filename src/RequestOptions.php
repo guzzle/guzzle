56be4f310e9724fb25315639caf7072c2c7f395e
Graham Campbell <hello@gjcampbell.co.uk>
2026-05-19 23:30:26 +0100

    Update documentation for 8.0

--- src/RequestOptions.php
+++ src/RequestOptions.php
@@ -5,7 +5,7 @@ namespace GuzzleHttp;
 /**
  * This class contains a list of built-in Guzzle request options.
  *
- * @see https://github.com/guzzle/guzzle/blob/7.11/docs/request-options.md
+ * @see https://github.com/guzzle/guzzle/blob/8.0/docs/request-options.md
  */
 final class RequestOptions
 {
