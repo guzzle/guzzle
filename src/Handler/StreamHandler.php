43b2d400a3e1e54f6a6cb7edc8cccc68e96bc687
Graham Campbell <hello@gjcampbell.co.uk>
2026-05-19 15:50:21 +0100

    Remove unnecessary stream header annotation

--- src/Handler/StreamHandler.php
+++ src/Handler/StreamHandler.php
@@ -352,7 +352,6 @@ class StreamHandler
 
                 // See https://wiki.php.net/rfc/deprecations_php_8_5#deprecate_the_http_response_header_predefined_variable
                 if (function_exists('http_get_last_response_headers')) {
-                    /** @var list<string>|null */
                     $http_response_header = \http_get_last_response_headers();
                 }
 
