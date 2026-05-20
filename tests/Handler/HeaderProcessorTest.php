620f208192e49745702e8d2c7a2607116828579c
Graham Campbell <hello@gjcampbell.co.uk>
2026-05-20 15:33:37 +0100

    Cover header processor errors

--- tests/Handler/HeaderProcessorTest.php
+++ tests/Handler/HeaderProcessorTest.php
@@ -17,12 +17,32 @@ class HeaderProcessorTest extends TestCase
             'Ignored: header',
             'HTTP/1.1 200 OK',
             'X-Foo: bar',
+            'X-Foo: baz',
+            'X-Bar: qux',
         ]);
 
         self::assertSame('1.1', $version);
         self::assertSame(200, $status);
         self::assertSame('OK', $reason);
-        self::assertSame(['X-Foo' => ['bar']], $headers);
+        self::assertSame(['X-Foo' => ['bar', 'baz'], 'X-Bar' => ['qux']], $headers);
+    }
+
+    public function testRejectsEmptyHeaderData(): void
+    {
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessage('Expected a non-empty array of header data');
+
+        HeaderProcessor::parseHeaders([]);
+    }
+
+    public function testRejectsMissingStatusCode(): void
+    {
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessage('HTTP status code missing from header data');
+
+        HeaderProcessor::parseHeaders([
+            'HTTP/1.1',
+        ]);
     }
 
     public function testRejectsMalformedStatusCode(): void
