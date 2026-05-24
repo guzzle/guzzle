b50bbfbb1856888dace423941cd05bac2dba03f8
Graham Campbell <GrahamCampbell@users.noreply.github.com>
2026-05-24 16:59:07 +0100

    Enable strict types in low-risk files (#3470)

--- src/Exception/GuzzleException.php
+++ src/Exception/GuzzleException.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace GuzzleHttp\Exception;
 
 use Psr\Http\Client\ClientExceptionInterface;
--- src/MessageFormatterInterface.php
+++ src/MessageFormatterInterface.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace GuzzleHttp;
 
 use Psr\Http\Message\RequestInterface;
