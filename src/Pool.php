b58a2d4d8e40d905062cf7ddb6a6b1e2617bfd1f
Graham Campbell <GrahamCampbell@users.noreply.github.com>
2026-05-29 16:39:01 +0100

    Remove Pool deprecation shim (#3541)

--- src/Pool.php
+++ src/Pool.php
@@ -131,18 +131,6 @@ class Pool implements PromisorInterface
             $opts = [];
         }
 
-        if (!\is_iterable($requests)) {
-            \trigger_deprecation(
-                'guzzlehttp/guzzle',
-                '7.11',
-                'Passing a non-iterable request collection to %s::__construct() or %s::batch() is deprecated; guzzlehttp/guzzle 8.0 will require an iterable.',
-                __CLASS__,
-                __CLASS__
-            );
-
-            $requests = [$requests];
-        }
-
         $requestGenerator = static function () use ($requests, $client, $opts): \Generator {
             foreach ($requests as $key => $rfn) {
                 if ($rfn instanceof RequestInterface) {
