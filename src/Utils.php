80ec84fbc0a861592f3cec02067ab06307c7d972
Graham Campbell <GrahamCampbell@users.noreply.github.com>
2026-06-09 00:10:04 +0100

    Remove Utils describeType (#3610)

--- src/Utils.php
+++ src/Utils.php
@@ -23,32 +23,6 @@ final class Utils
     }
 
     /**
-     * Debug function used to describe the provided value type and class.
-     *
-     * @param mixed $input
-     *
-     * @return string Returns a string containing the type of the variable and
-     *                if a class is provided, the class name.
-     */
-    public static function describeType($input): string
-    {
-        switch (\gettype($input)) {
-            case 'object':
-                return 'object('.\get_class($input).')';
-            case 'array':
-                return 'array('.\count($input).')';
-            default:
-                \ob_start();
-                \var_dump($input);
-                // normalize float vs double
-                /** @var string $varDumpContent */
-                $varDumpContent = \ob_get_clean();
-
-                return \str_replace('double(', 'float(', \rtrim($varDumpContent));
-        }
-    }
-
-    /**
      * Parses an array of header lines into an associative array of headers.
      *
      * @param iterable $lines Header lines array of strings in the following
