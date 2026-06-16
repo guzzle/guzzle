b0d8da1772622ea8f5affab29b767b65806890de
Graham Campbell <hello@gjcampbell.co.uk>
2026-06-16 23:06:47 +0100

    Correct links

--- src/Utils.php
+++ src/Utils.php
@@ -209,14 +209,14 @@ final class Utils
 No system CA bundle could be found in any of the the common system locations.
 PHP versions earlier than 5.6 are not properly configured to use the system's
 CA bundle by default. In order to verify peer certificates, you will need to
-supply the path on disk to a certificate bundle to the 'verify' request
-option: https://github.com/guzzle/guzzle/blob/7.11/docs/request-options.md#verify. If
+supply the path on disk to a certificate bundle to the 'verify' request option:
+https://github.com/guzzle/guzzle/blob/7.12/docs/request-options.md#verify. If
 you do not need a specific certificate bundle, then Mozilla provides a commonly
 used CA bundle which can be downloaded here (provided by the maintainer of
-cURL): https://curl.haxx.se/ca/cacert.pem. Once you have a CA bundle available
-on disk, you can set the 'openssl.cafile' PHP ini setting to point to the path
-to the file, allowing you to omit the 'verify' request option. See
-https://curl.haxx.se/docs/sslcerts.html for more information.
+cURL): https://curl.se/ca/cacert.pem. Once you have a CA bundle available on
+disk, you can set the 'openssl.cafile' PHP ini setting to point to the path to
+the file, allowing you to omit the 'verify' request option. See
+https://curl.se/docs/sslcerts.html for more information.
 EOT
         );
     }
