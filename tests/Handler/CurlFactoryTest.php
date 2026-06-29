8d03bf2c2d6c94d586beaae871f43d1ac28a61dc
Graham Campbell <hello@gjcampbell.co.uk>
2026-06-29 19:58:46 +0100

    Validate cURL scheme support before protocol policy

--- tests/Handler/CurlFactoryTest.php
+++ tests/Handler/CurlFactoryTest.php
@@ -907,6 +907,36 @@ class CurlFactoryTest extends TestCase
         $f->create(new Psr7\Request('GET', 'http://example.com'), ['protocols' => ['https']]);
     }
 
+    public function testRejectsUnsupportedScheme(): void
+    {
+        $f = new CurlFactory(3);
+
+        $this->expectException(RequestException::class);
+        $this->expectExceptionMessage("The scheme 'ftp' is not supported.");
+
+        $f->create(new Psr7\Request('GET', 'ftp://example.com'), []);
+    }
+
+    public function testRejectsUnsupportedSchemeBeforeMissingHost(): void
+    {
+        $f = new CurlFactory(3);
+
+        $this->expectException(RequestException::class);
+        $this->expectExceptionMessage("The scheme 'file' is not supported.");
+
+        $f->create(new Psr7\Request('GET', 'file:///etc/passwd'), []);
+    }
+
+    public function testProtocolsOptionRejectsBeforeMissingHost(): void
+    {
+        $f = new CurlFactory(3);
+
+        $this->expectException(RequestException::class);
+        $this->expectExceptionMessage('not allowed by the protocols request option');
+
+        $f->create(new Psr7\Request('GET', 'http:/generate_204'), ['protocols' => ['https']]);
+    }
+
     /**
      * @dataProvider uriMissingSchemeOrHostProvider
      */
