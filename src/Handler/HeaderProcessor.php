68d977aff102b8d09b1d6fd05d8cc9ed36358ee2
Graham Campbell <GrahamCampbell@users.noreply.github.com>
2026-07-05 02:41:20 +0100

    Validate collected HTTP trailer fields (#3756)

--- src/Handler/HeaderProcessor.php
+++ src/Handler/HeaderProcessor.php
@@ -57,6 +57,26 @@ final class HeaderProcessor
         return [$version, (int) $status, $parts[2] ?? null, Utils::headersFromLines($headers)];
     }
 
+    public static function isStatusLineCandidate(string $line): bool
+    {
+        return \preg_match('/^HTTP\/[0-9]+(?:\.[0-9]+)? [0-9]{3}(?: [^\r\n]*)?(?:\r\n|\r|\n)?$/iD', $line) === 1;
+    }
+
+    public static function isValidHeaderFieldLine(string $line): bool
+    {
+        $parts = \explode(':', $line, 2);
+
+        if (!isset($parts[1])) {
+            return false;
+        }
+
+        if (!\preg_match('/^[a-zA-Z0-9\'`#$%&*+.^_|~!-]+$/D', $parts[0])) {
+            return false;
+        }
+
+        return \preg_match('/^[\x20\x09\x21-\x7E\x80-\xFF]*(?:\r\n|\r|\n)?$/D', \trim($parts[1], " \t")) === 1;
+    }
+
     /**
      * @param non-empty-list<string> $headers
      *
@@ -67,7 +87,7 @@ final class HeaderProcessor
         $lastStatusLine = 0;
 
         foreach ($headers as $index => $line) {
-            if (\preg_match('/^HTTP\/\S+\s+/i', $line)) {
+            if (self::isStatusLineCandidate($line)) {
                 $lastStatusLine = $index;
             }
         }
